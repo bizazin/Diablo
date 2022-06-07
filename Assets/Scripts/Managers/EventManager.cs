a34138a8cf799a05a3d7b5aeddf752b4e7ff964e
Ruslan <ruslan.makhn@gmail.com>
2022-06-07 14:35:09 +0300

    [ADD] Добавлен менеджер ключей и ивент менеджер

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static EventManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(Instance.gameObject);
+        }
+    }
+
+    #endregion
+
+    public UnityEvent OnDefaultEvent;
+}
