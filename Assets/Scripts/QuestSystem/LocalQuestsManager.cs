d5e39e79ac21b8078c5d924e87f3fad6afc5bfbb
Danil <nesindan@gmail.com>
2022-07-04 14:40:35 +0300

    [add] сохранение мейн квестов

--- Assets/Scripts/QuestSystem/LocalQuestsManager.cs
+++ Assets/Scripts/QuestSystem/LocalQuestsManager.cs
@@ -12,10 +12,7 @@ public class LocalQuestsManager : MonoBehaviour, ICanBeSaved
     private void Awake()
     {
         if (Instance != null)
-        {
-            Debug.LogWarning("More than one instance of Inventory is found!");
             return;
-        }
         Instance = this;
     }
     #endregion
