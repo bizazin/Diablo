686209c707a56abdd44b9e57117599fc91ed2bb0
Egor Babaev <babaev1302@gmail.com>
2022-07-19 23:49:07 +0300

    [fix] after refactoring

--- Assets/Scripts/UI/TargetPointer.cs
+++ Assets/Scripts/UI/TargetPointer.cs
@@ -1,99 +1,91 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using System;
 
 public class TargetPointer : MonoBehaviour
 {
+    public static TargetPointer Instance;
 
-	#region Singleton
+    public Transform Target;
+    [SerializeField] private RectTransform PointerUI;
+    [SerializeField] private Sprite PointerIcon;
+    [SerializeField] private Sprite OutOfScreenIcon;
+    [SerializeField] private float InterfaceScale = 100;
 
-	public static TargetPointer Instance;
+    private Vector3 startPointerSize;
+    private Camera mainCamera;
 
-	private void Awake()
-	{
-		if (Instance != null)
-			return;
-		Instance = this;
-	}
-	#endregion
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory is found!");
+            return;
+        }
+        Instance = this;
 
-	[SerializeField] private RectTransform pointerUI;
-	[SerializeField] private Sprite outOfScreenIcon; 	
-	[SerializeField] private Sprite pointerIcon;
-	[SerializeField] private float interfaceScale;
+        startPointerSize = PointerUI.sizeDelta;
+        mainCamera = Camera.main;
+    }
+    private void LateUpdate()
+    {
+        if (Target == null)
+            return;
 
-	private Vector3 startPointerSize;
-	private Camera mainCamera;
-	
-	public Transform Target;
+        Vector3 realPos = mainCamera.WorldToScreenPoint(Target.position);
+        Rect rect = new Rect(0, 0, Screen.width, Screen.height);
 
-	private void Start()
-	{
-		interfaceScale = 100;
-		startPointerSize = pointerUI.sizeDelta;
-		mainCamera = Camera.main;		
-	}
+        Vector3 outPos = realPos;
+        float direction = 1;
 
-	private void LateUpdate()
-	{
-		if (Target == null)
-			return;
-		
-		Vector3 realPos = mainCamera.WorldToScreenPoint(Target.position); 
-		Rect rect = new Rect(0, 0, Screen.width, Screen.height);
+        PointerUI.GetComponent<Image>().sprite = OutOfScreenIcon;
 
-		Vector3 outPos = realPos;
-		float direction = 1;
+        if (!IsBehind(Target.position))
+            if (rect.Contains(realPos))
+                 PointerUI.GetComponent<Image>().sprite = PointerIcon;
+        else
+        {
+            realPos = -realPos;
+            outPos = new Vector3(realPos.x, 0, 0);
+            if (mainCamera.transform.position.y < Target.position.y)
+            {
+                direction = -1;
+                outPos.y = Screen.height;
+            }
+        }
 
-		pointerUI.GetComponent<Image>().sprite = outOfScreenIcon;
+        float offset = PointerUI.sizeDelta.x / 2;
+        outPos.x = Mathf.Clamp(outPos.x, offset, Screen.width - offset);
+        outPos.y = Mathf.Clamp(outPos.y, offset, Screen.height - offset);
 
-		if (!IsBehind(Target.position)) 
-			if (rect.Contains(realPos)) 
-			 	pointerUI.GetComponent<Image>().sprite = pointerIcon;
-		else 
-		{
-			realPos = -realPos;
-			outPos = new Vector3(realPos.x, 0, 0); 
-			if (mainCamera.transform.position.y < Target.position.y)
-			{
-				direction = -1;
-				outPos.y = Screen.height; 				
-			}
-		}
-		
-		float offset = pointerUI.sizeDelta.x / 2;
-		outPos.x = Mathf.Clamp(outPos.x, offset, Screen.width - offset);
-		outPos.y = Mathf.Clamp(outPos.y, offset, Screen.height - offset);
+        Vector3 pos = realPos - outPos;
 
-		Vector3 pos = realPos - outPos;  
+        RotatePointer(direction * pos);
 
-		RotatePointer(direction * pos);
+        PointerUI.sizeDelta = new Vector2(startPointerSize.x / 100 * InterfaceScale, startPointerSize.y / 100 * InterfaceScale);
+        PointerUI.anchoredPosition = outPos;
+    }
 
-		pointerUI.sizeDelta = new Vector2(startPointerSize.x / 100 * interfaceScale, startPointerSize.y / 100 * interfaceScale);
-		pointerUI.anchoredPosition = outPos;
-	}
+    private bool IsBehind(Vector3 point)
+    {
+        Vector3 forward = mainCamera.transform.TransformDirection(Vector3.forward);
+        Vector3 toOther = point - mainCamera.transform.position;
+        if (Vector3.Dot(forward, toOther) < 0) return true;
+        return false;
+    }
 
-	private bool IsBehind(Vector3 point) 
-	{		
-		Vector3 forward = mainCamera.transform.TransformDirection(Vector3.forward);
-		Vector3 toOther = point - mainCamera.transform.position;
-		return Vector3.Dot(forward, toOther) < 0;
-	}
+    private void RotatePointer(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        PointerUI.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 
-	private void RotatePointer(Vector2 direction) 
-	{		
-		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-		pointerUI.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-	}
-
-	public void ToggleTarget(bool state)
-	{
-		var image = GetComponent<Image>();
-		var tempColor = image.color;
-		tempColor.a = Convert.ToInt32(state);
-		image.color = tempColor;
-		pointerUI.GetComponent<Image>().color = tempColor;
-	}
-
+    public void ToggleTarget(bool state)
+    {
+        var image = GetComponent<Image>();
+        var tempColor = image.color;
+        tempColor.a = Convert.ToInt32(state);
+        image.color = tempColor;
+        PointerUI.GetComponent<Image>().color = tempColor;
+    }
 }
